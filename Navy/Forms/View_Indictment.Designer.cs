04f8a1e392ee9094500c6b1d17e06eac3b05208a
THEERANAN-MSI\Theeranan <theeranan008@gmail.com>
2018-06-07 09:00:32 +0700

    reverse

--- Navy/Forms/View_Indictment.Designer.cs
+++ Navy/Forms/View_Indictment.Designer.cs
@@ -50,20 +50,18 @@
             // panelDetail
             // 
             this.panelDetail.Controls.Add(this.button1);
-            this.panelDetail.Location = new System.Drawing.Point(9, 118);
-            this.panelDetail.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.panelDetail.Location = new System.Drawing.Point(6, 67);
             this.panelDetail.Name = "panelDetail";
-            this.panelDetail.Size = new System.Drawing.Size(1203, 562);
+            this.panelDetail.Size = new System.Drawing.Size(802, 318);
             this.panelDetail.TabIndex = 6;
             // 
             // button1
             // 
             this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button1.Location = new System.Drawing.Point(1158, 513);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.button1.Location = new System.Drawing.Point(772, 290);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(45, 49);
+            this.button1.Size = new System.Drawing.Size(30, 28);
             this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -72,11 +70,10 @@
             // 
             this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtName.Location = new System.Drawing.Point(4, 6);
-            this.txtName.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.txtName.Location = new System.Drawing.Point(3, 3);
             this.txtName.Name = "txtName";
             this.txtName.ReadOnly = true;
-            this.txtName.Size = new System.Drawing.Size(1185, 39);
+            this.txtName.Size = new System.Drawing.Size(790, 31);
             this.txtName.TabIndex = 1;
             this.txtName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -87,12 +84,11 @@
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
             this.tableLayoutPanel1.Controls.Add(this.txtName, 0, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(4, 17);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 10);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1194, 75);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(796, 42);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // linkLabel1
@@ -101,10 +97,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLabel1.Location = new System.Drawing.Point(1097, 252);
-            this.linkLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.linkLabel1.Location = new System.Drawing.Point(731, 142);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(75, 36);
+            this.linkLabel1.Size = new System.Drawing.Size(59, 29);
             this.linkLabel1.TabIndex = 7;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "พิมพ์";
@@ -113,11 +108,9 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.tableLayoutPanel1);
-            this.groupBox1.Location = new System.Drawing.Point(9, 6);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.groupBox1.Location = new System.Drawing.Point(6, 3);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 6, 4, 6);
-            this.groupBox1.Size = new System.Drawing.Size(1203, 102);
+            this.groupBox1.Size = new System.Drawing.Size(802, 58);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             // 
@@ -129,33 +122,30 @@
             this.gv_Indictment.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
             this.gv_Indictment.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
             this.gv_Indictment.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.gv_Indictment.Location = new System.Drawing.Point(4, 29);
-            this.gv_Indictment.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.gv_Indictment.Location = new System.Drawing.Point(3, 16);
+            this.gv_Indictment.Margin = new System.Windows.Forms.Padding(4);
             this.gv_Indictment.MultiSelect = false;
             this.gv_Indictment.Name = "gv_Indictment";
             this.gv_Indictment.ReadOnly = true;
             this.gv_Indictment.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.gv_Indictment.Size = new System.Drawing.Size(1195, 408);
+            this.gv_Indictment.Size = new System.Drawing.Size(796, 231);
             this.gv_Indictment.TabIndex = 3;
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.gv_Indictment);
-            this.groupBox2.Location = new System.Drawing.Point(9, 691);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.groupBox2.Location = new System.Drawing.Point(6, 391);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 6, 4, 6);
-            this.groupBox2.Size = new System.Drawing.Size(1203, 443);
+            this.groupBox2.Size = new System.Drawing.Size(802, 250);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "ข้อหา";
             // 
             // Add_Indictment
             // 
-            this.Add_Indictment.Location = new System.Drawing.Point(18, 1144);
-            this.Add_Indictment.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.Add_Indictment.Location = new System.Drawing.Point(12, 647);
             this.Add_Indictment.Name = "Add_Indictment";
-            this.Add_Indictment.Size = new System.Drawing.Size(112, 40);
+            this.Add_Indictment.Size = new System.Drawing.Size(75, 23);
             this.Add_Indictment.TabIndex = 8;
             this.Add_Indictment.Text = "เพิ่มข้อหา";
             this.Add_Indictment.UseVisualStyleBackColor = true;
@@ -163,10 +153,9 @@
             // 
             // Remove_Indictment
             // 
-            this.Remove_Indictment.Location = new System.Drawing.Point(968, 1144);
-            this.Remove_Indictment.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.Remove_Indictment.Location = new System.Drawing.Point(645, 647);
             this.Remove_Indictment.Name = "Remove_Indictment";
-            this.Remove_Indictment.Size = new System.Drawing.Size(112, 40);
+            this.Remove_Indictment.Size = new System.Drawing.Size(75, 23);
             this.Remove_Indictment.TabIndex = 9;
             this.Remove_Indictment.Text = "ลบข้อหา";
             this.Remove_Indictment.UseVisualStyleBackColor = true;
@@ -174,10 +163,9 @@
             // 
             // Edit_Indictment
             // 
-            this.Edit_Indictment.Location = new System.Drawing.Point(1089, 1144);
-            this.Edit_Indictment.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.Edit_Indictment.Location = new System.Drawing.Point(726, 647);
             this.Edit_Indictment.Name = "Edit_Indictment";
-            this.Edit_Indictment.Size = new System.Drawing.Size(112, 40);
+            this.Edit_Indictment.Size = new System.Drawing.Size(75, 23);
             this.Edit_Indictment.TabIndex = 10;
             this.Edit_Indictment.Text = "แก้ไข";
             this.Edit_Indictment.UseVisualStyleBackColor = true;
@@ -185,9 +173,9 @@
             // 
             // View_Indictment
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1220, 1055);
+            this.ClientSize = new System.Drawing.Size(813, 678);
             this.Controls.Add(this.Edit_Indictment);
             this.Controls.Add(this.Remove_Indictment);
             this.Controls.Add(this.Add_Indictment);
@@ -195,8 +183,6 @@
             this.Controls.Add(this.panelDetail);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.groupBox1);
-            this.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.Name = "View_Indictment";
             this.Text = "ข้อมูลข้อหาทหาร";
             this.panelDetail.ResumeLayout(false);
