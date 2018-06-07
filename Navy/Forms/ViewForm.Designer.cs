d0ccbf104dc403b3c61f1f10d18fb80570d4de97
THEERANAN-MSI\Theeranan <theeranan008@gmail.com>
2018-06-07 10:00:34 +0700

    เพิ่มการปริ้นหน้าจอ

--- Navy/Forms/ViewForm.Designer.cs
+++ Navy/Forms/ViewForm.Designer.cs
@@ -45,19 +45,21 @@
             // 
             this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtName.Location = new System.Drawing.Point(3, 3);
+            this.txtName.Location = new System.Drawing.Point(4, 4);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4);
             this.txtName.Name = "txtName";
             this.txtName.ReadOnly = true;
-            this.txtName.Size = new System.Drawing.Size(471, 31);
+            this.txtName.Size = new System.Drawing.Size(628, 39);
             this.txtName.TabIndex = 1;
             this.txtName.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // panelDetail
             // 
             this.panelDetail.Controls.Add(this.linkLabel2);
-            this.panelDetail.Location = new System.Drawing.Point(0, 98);
+            this.panelDetail.Location = new System.Drawing.Point(0, 121);
+            this.panelDetail.Margin = new System.Windows.Forms.Padding(4);
             this.panelDetail.Name = "panelDetail";
-            this.panelDetail.Size = new System.Drawing.Size(802, 584);
+            this.panelDetail.Size = new System.Drawing.Size(1069, 719);
             this.panelDetail.TabIndex = 3;
             // 
             // linkLabel2
@@ -65,9 +67,10 @@
             this.linkLabel2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.linkLabel2.AutoSize = true;
             this.linkLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLabel2.Location = new System.Drawing.Point(552, 193);
+            this.linkLabel2.Location = new System.Drawing.Point(736, 238);
+            this.linkLabel2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.linkLabel2.Name = "linkLabel2";
-            this.linkLabel2.Size = new System.Drawing.Size(153, 29);
+            this.linkLabel2.Size = new System.Drawing.Size(191, 36);
             this.linkLabel2.TabIndex = 2;
             this.linkLabel2.TabStop = true;
             this.linkLabel2.Text = "[คัดลอกรูปถ่าย]";
@@ -83,21 +86,23 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
             this.tableLayoutPanel1.Controls.Add(this.txtName, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.linkStatus, 1, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 16);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(4, 20);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 31F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(796, 61);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 38F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1061, 75);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // linkStatus
             // 
             this.linkStatus.AutoSize = true;
             this.linkStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkStatus.Location = new System.Drawing.Point(480, 0);
+            this.linkStatus.Location = new System.Drawing.Point(640, 0);
+            this.linkStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.linkStatus.Name = "linkStatus";
-            this.linkStatus.Size = new System.Drawing.Size(93, 29);
+            this.linkStatus.Size = new System.Drawing.Size(115, 36);
             this.linkStatus.TabIndex = 0;
             this.linkStatus.TabStop = true;
             this.linkStatus.Text = "[Status]";
@@ -108,9 +113,11 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.tableLayoutPanel1);
-            this.groupBox1.Location = new System.Drawing.Point(0, 14);
+            this.groupBox1.Location = new System.Drawing.Point(0, 17);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(802, 80);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(1069, 98);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             // 
@@ -120,9 +127,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLabel1.Location = new System.Drawing.Point(725, 0);
+            this.linkLabel1.Location = new System.Drawing.Point(967, 0);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(59, 29);
+            this.linkLabel1.Size = new System.Drawing.Size(75, 36);
             this.linkLabel1.TabIndex = 4;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "พิมพ์";
@@ -133,9 +141,10 @@
             // 
             this.linkLabel3.AutoSize = true;
             this.linkLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLabel3.Location = new System.Drawing.Point(531, 0);
+            this.linkLabel3.Location = new System.Drawing.Point(708, 0);
+            this.linkLabel3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.linkLabel3.Name = "linkLabel3";
-            this.linkLabel3.Size = new System.Drawing.Size(200, 29);
+            this.linkLabel3.Size = new System.Drawing.Size(257, 36);
             this.linkLabel3.TabIndex = 4;
             this.linkLabel3.TabStop = true;
             this.linkLabel3.Text = "สำเนาบัตรประชาชน";
@@ -144,13 +153,14 @@
             // 
             // ViewForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(802, 602);
+            this.ClientSize = new System.Drawing.Size(1069, 741);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.panelDetail);
             this.Controls.Add(this.linkLabel3);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ViewForm";
             this.Text = "ประวัติทหาร";
             this.Load += new System.EventHandler(this.ViewForm_Load);
